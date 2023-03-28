805371945f3c2908264ade79653034fd703776bf
Andrei Ignatov <super.dronshamann@gmail.com>
2023-03-28 17:30:02 +0400

    5 iteration

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-# Done! Congratulations on your new bot. You will find it at t.me/BotoFed_bot. You can now add a description, about section
-# and profile picture for your bot, see /help for a list of commands. By the way, when you've finished creating your cool bot,
-# ping our Bot Support if you want a better username for it. Just make sure the bot is fully operational before you do this.
+# Done! Congratulations on your new bot. You will find it at t.me/BotoFed_bot. You can now add a description, about section and profile picture for your bot, see /help for a list of commands. By the way, when you've finished creating your cool bot,ping our Bot Support if you want a better username for it. Just make sure the bot is fully operational before you do this.
 #
 # Use this token to access the HTTP API:
 # 6256786801:AAF2nxsgMCek4TjCOSNWIXe6GglsEy8ftZ0
@@ -13,12 +11,12 @@ import telebot  # import telebot with api
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
-# ответна команду /start
+# ответ на команду /start
 @bot.message_handler(commands=["start"])
 def cmd_start(message):
-    bot.reply_to(message, "Ну и зачем ты команды запускаешь???")
+    bot.reply_to(message, "Ну и зачем ты это запускаешь???")
 
-@bot.message_handler(commands=["text"])
+@bot.message_handler(content_types=["text"])
 def bot_msg_text(message):
     if message.text.startswith("/"):
         bot.send_message(message.chat.id, "Команда не доступна")
@@ -26,8 +24,6 @@ def bot_msg_text(message):
         bot.send_message(message.chat.id, "Это не команда")
 
 if __name__ == '__main__':
-    print("Оно живое!!!")
+    print("Оно живое!")
     bot.infinity_polling()
     print("Конец")
-
-
