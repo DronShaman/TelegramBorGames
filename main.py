2a989d432e18b4dd5b513ff3f6f09b7faa6b74fb
Andrei Ignatov <super.dronshamann@gmail.com>
2023-04-06 12:40:53 +0400

    добавляю код для трово, пока не рабоатет

--- main.py
+++ main.py
@@ -8,18 +8,24 @@
 
 from config import *  # import my token
 import telebot  # import telebot with api
+import requests
+import time
 
+# Задаем токен бота и ID чата, куда будут отправляться оповещения
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
+chat_id = "YOUR_CHAT_ID"
 
 # ответ на команду /start
 @bot.message_handler(commands=["start"])
 def cmd_start(message):
-    bot.reply_to(message, "Фед Быстрый интернет запущен")
+    bot.reply_to(message, "Бот Фед Быстрый интернет запущен")
 
 @bot.message_handler(content_types=["text"])
 def bot_msg_text(message):
     if message.text.startswith("/"):
         bot.send_message(message.chat.id, "Меня не взломать, фед бессмертный")
+    elif message.text.startswith("Привет") or message.text.startswith("Ку") or message.text.startswith("Салют"):
+        bot.send_message(message.chat.id, "Здорово, заебал")
     else:
         bot.send_message(message.chat.id, "Не знаю такой команды")
 
@@ -27,3 +33,28 @@ if __name__ == '__main__':
     print("Оно живое!")
     bot.infinity_polling()
     print("Конец")
+
+# Задаем токен бота и ID чата, куда будут отправляться оповещения
+chat_id = "https://t.me/dronshaman"
+
+# Задаем URL для получения информации о стриме на Trovo
+stream_url = "https://trovo.live/s/DronShaman"
+
+# Задаем интервал между проверками начала трансляции в секундах
+check_interval = 60
+
+# Основной цикл проверки начала трансляции
+while True:
+    # Отправляем GET запрос к API Trovo для получения информации о стриме
+    response = requests.get(stream_url)
+    data = response.json()
+
+    # Проверяем, началась ли трансляция
+    if data["streamInfo"]["streamStatus"] == "Live":
+        # Если трансляция началась, отправляем сообщение в Telegram
+        message = "Началась трансляция на Trovo!"
+        url = f"https://api.telegram.org/bot{bot}/sendMessage?chat_id={chat_id}&text={message}"
+        requests.get(url)
+
+    # Ждем заданное количество секунд перед следующей проверкой
+    time.sleep(check_interval)
